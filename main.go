4f5e11c3c12a060cdd8065cfd1ff922c16bc4227
nt-nabeel <nt.nabeel027@gmail.com>
2022-03-21 22:22:27 +0700

    fix: crawl csr

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
 )
+
+func main() {
+	ssr := CrawlLink("https://ssr-assessment-miqdadyyy.vercel.app/")
+	csr := CrawlLink("https://csr-assessment-miqdadyyy.vercel.app/")
+
+	fmt.Println("")
+	fmt.Println(ssr)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(csr)
+	fmt.Println("")
+}
 
 type Link struct {
 	url   string
@@ -32,22 +45,59 @@ func CrawlLink(url string) []Link {
 
 	var links []Link
 	for _, node := range nodes {
-		var link Link
 		for _, attribute := range node.Attr {
 			if attribute.Key == "href" {
+				var link Link
 				link.url = attribute.Val
+				link.title = getText(node)
+				links = append(links, link)
+			}
+			if attribute.Key == "src" {
+				nameSlice := strings.Split(attribute.Val, ".")
+				isApp := strings.HasSuffix(nameSlice[0], "app")
+				if isApp {
+					links = getLinkFromJS(url+attribute.Val, links)
+				}
 			}
 		}
-		link.title = getText(node)
-
-		links = append(links, link)
 	}
 
 	return links
 }
 
+func getLinkFromJS(url string, links []Link) []Link {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error")
+		panic(err.Error)
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error")
+		panic(err.Error)
+	}
+	data := strings.Split(string(body), ";")
+	for _, c := range data {
+		if len(strings.Split(c, "links=")) == 2 {
+			_links := strings.Split(c, "links=")[1]
+			_links = _links[2:(len(_links) - 2)]
+			linkObj := strings.Split(_links, "},{")
+			for _, _link := range linkObj {
+				_link = _link[5:]
+				_data := strings.Split(_link, `",title:"`)
+
+				var link Link
+				link.url = _data[0]
+				link.title = _data[1][:(len(_data[1]) - 1)]
+				links = append(links, link)
+			}
+		}
+	}
+	return links
+}
+
 func getNode(node *html.Node) []*html.Node {
-	if node.Type == html.ElementNode && node.Data == "a" {
+	if node.Type == html.ElementNode && (node.Data == "a" || node.Data == "script") {
 		return []*html.Node{
 			node,
 		}
